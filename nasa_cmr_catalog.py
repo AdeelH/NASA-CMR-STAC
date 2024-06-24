796e67a3e6eb4a3a05a72be386c4bea2aea09096
Adeel Hassan <ahassan@element84.com>
2024-06-24 14:37:55 -0400

    update script to handle pagination in collection search results

--- nasa_cmr_catalog.py
+++ nasa_cmr_catalog.py
@@ -1,95 +1,133 @@
+from typing import Any
+from os import makedirs
+from os.path import join, exists
 import json
-import os
-import pandas as pd
-from pystac_client import Client
+from argparse import ArgumentParser
 
-out_dir = "datasets"
+from tqdm.auto import tqdm
+import requests
 
-url = "https://cmr.earthdata.nasa.gov/stac"
+URL = 'https://cmr.earthdata.nasa.gov/stac/'
+TIMEOUT = 20
 
-root = Client.open(url, headers=[])
 
-catalogs = []
+def main(download_dir: str,
+         full: bool = False,
+         force: bool = False,
+         catalogs_to_skip: list[str] = []) -> None:
+    catalogs = get_catalogs(URL)
+    save_catalogs(catalogs, download_dir)
 
-for link in root.get_child_links():
-    if link.rel == "child":
-        catalogs.append(link.target)
+    with tqdm(catalogs, desc='Catalogs') as bar:
+        for catalog in bar:
+            if catalog in catalogs_to_skip:
+                print(f'Skipping {catalog["title"]}. Ignored via --skip.')
+            fetch_collections(catalog, download_dir, full=full, force=force)
 
-datasets = []
 
-for catalog in catalogs:
+def fetch_collections(catalog: dict,
+                      download_dir: str,
+                      full: bool = False,
+                      force: bool = False) -> None:
+    title = catalog['title']
+    download_path = join(download_dir, f'collections_{title}.json')
+    if not force and exists(download_path):
+        print(f'Skipping {title}. File already exists.')
+        return
+    collections = get_all_collections(catalog, full=full)
+    json_to_file(collections, download_path)
 
-    try:
-        cat = Client.open(catalog, headers=[])
-        print(cat.title)
-        for collection in cat.get_all_collections():
-            data = collection.to_dict()
-            print(data["id"])
-            dataset = {}
-            output = out_dir + "/" + data["id"].replace("/", "_") + ".json"
-            if not os.path.exists(os.path.dirname(output)):
-                os.makedirs(os.path.dirname(output))
-            with open(output, "w") as f:
-                json.dump(data, f, indent=4)
 
-            dataset["id"] = data["id"].strip()
-            dataset["title"] = data["title"].strip()
-            dataset["catalog"] = cat.title.strip()
+def get_catalogs(url: str) -> list[dict]:
+    r = requests.get(url, timeout=TIMEOUT)
+    data = r.json()
+    catalogs = [link for link in data['links'] if link['rel'] == 'child']
+    return catalogs
 
-            start_date = data["extent"]["temporal"]["interval"][0][0]
-            end_date = data["extent"]["temporal"]["interval"][0][1]
 
-            if start_date is not None:
-                dataset["state_date"] = start_date.split("T")[0]
-            else:
-                dataset["state_date"] = ""
+def get_all_collections(catalog: dict, full: bool = False) -> list[dict]:
+    collections = []
+    url = join(catalog['href'], 'collections')
+    title = catalog['title']
+    with tqdm(desc=f'Fetching {title} collections') as bar:
+        while url is not None:
+            r = requests.get(url, timeout=TIMEOUT)
+            if r.status_code != 200:
+                print(f'Status code: {r.status_code}')
+                break
+            data = r.json()
+            page_collections = data.get('collections', [])
+            if not full:
+                page_collections = [{k: c[k]
+                                     for k in ['id', 'title']}
+                                    for c in page_collections]
+            collections.extend(page_collections)
+            bar.update()
+            bar.set_postfix_str(f'#collections={len(collections)}')
+            url = get_next_page_url(data)
+    return collections
 
-            if end_date is not None:
-                dataset["end_date"] = end_date.split("T")[0]
-            else:
-                dataset["end_date"] = ""
-            dataset["bbox"] = ", ".join(
-                [str(coord) for coord in data["extent"]["spatial"]["bbox"][0]]
-            )
 
-            url = ""
-            metadata = ""
-            href = ""
+def get_next_page_url(data: dict) -> str | None:
+    for link in data.get('links', []):
+        if link.get('rel') == 'next':
+            return link.get('href')
+    return None
 
-            for l in data["links"]:
-                if l["rel"] == "about":
-                    metadata = l["href"]
-                if l["rel"] == "self":
-                    href = l["href"]
-                if l["rel"] == "via":
-                    url = l["href"]
 
-            dataset["url"] = url
-            dataset["metadata"] = metadata
-            dataset["href"] = href
+def json_to_file(obj: Any, path: str):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(obj, f, indent=0)
 
-            dataset["description"] = (
-                data["description"]
-                .replace("\n", " ")
-                .replace("\r", " ")
-                .replace("\\u", " ")
-                .replace("                 ", " ")
-            )
 
-            dataset["license"] = data["license"]
+def save_catalogs(catalogs: list[dict], download_dir: str) -> None:
+    catalog_names = sorted([c['title'] for c in catalogs])
+    catalogs_json_path = join(download_dir, 'catalogs.json')
+    json_to_file(catalogs, catalogs_json_path)
+    catalog_names_path = join(download_dir, 'catalog_names.txt')
+    with open(catalog_names_path, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(catalog_names))
 
-            datasets.append(dataset)
-    except Exception as e:
-        print("Error: ", catalog)
-        print(e)
 
-print("Total datasets: ", len(datasets))
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        'download_dir', type=str, help='Path to download directory.')
+    parser.add_argument(
+        '--catalog',
+        type=str,
+        help='Download collections for a specific catalog.')
+    parser.add_argument(
+        '--full',
+        action='store_true',
+        help='Save the full collection metadata as opposed to '
+        'just ID and title.')
+    parser.add_argument(
+        '--force',
+        action='store_true',
+        help='Download files even if they already exist.')
+    parser.add_argument(
+        '--skip', type=str, help='Comma-separated names of catalogs to skip.')
 
-df = pd.DataFrame(datasets)
-df.sort_values(by=["id"], inplace=True)
-df.drop(columns=["href", "metadata"]).to_csv(
-    "nasa_cmr_catalog.tsv", index=False, sep="\t"
-)
-
-with open("nasa_cmr_catalog.json", "w") as f:
-    json.dump(df.to_dict("records"), f, indent=4)
+    opt = parser.parse_args()
+    download_dir = opt.download_dir
+    catalog = opt.catalog
+    force = opt.force
+    full = opt.full
+    catalogs_to_skip = opt.skip.split(',') if opt.skip is not None else []
+    makedirs(download_dir, exist_ok=True)
+    if catalog is not None:
+        catalogs = get_catalogs(URL)
+        save_catalogs(catalogs, download_dir)
+        catalog_names = [c['title'] for c in catalogs]
+        fetch_collections(
+            catalogs[catalog_names.index(catalog)],
+            download_dir,
+            full=full,
+            force=force)
+    else:
+        main(
+            download_dir=download_dir,
+            full=full,
+            force=force,
+            catalogs_to_skip=catalogs_to_skip)
